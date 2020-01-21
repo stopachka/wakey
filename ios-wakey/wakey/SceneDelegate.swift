a12d1592ceaaae6884735ecb6ce4423575bc4133
stopachka <stepan.parunashvili@airbnb.com>
2020-01-21 13:03:14 -0800

    refactor-dat

--- ios-wakey/wakey/SceneDelegate.swift
+++ ios-wakey/wakey/SceneDelegate.swift
@@ -13,8 +13,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
-            .environmentObject(SessionStore())
-            .environmentObject(UserInfoStore())
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
